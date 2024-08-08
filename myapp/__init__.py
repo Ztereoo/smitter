46642bf9f5322b500eb6e5df478feb63cb449bd5
stereo <airstarview@gmail.com>
2024-08-08 16:30:56 +0300

    templates created bootstrap attached to project

---
+++ myapp/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.secret_key='abrababra21{]!'
+# manager=LoginManager(app)
+db=SQLAlchemy(app)
+
+from myapp import models,routes
+
+with app.app_context():
+    db.create_all()
+
